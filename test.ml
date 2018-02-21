5fa9c13a8267bdeda4f480ba0f6a7eae6a96ec88
Mari Husain <maryamhusain98@gmail.com>
2018-02-20 21:06:01 -0500

    Wrote python test script, modified toplevel to not print out AST (use test.ml while working - it does what toplevel.ml used to do

--- test.ml
+++ test.ml
@@ -1,6 +1,5 @@
 (* Top-level code to drive the scanner and parser for Joel on some input.
-   Scan and parse the input. Does not print the AST - only prints "Success"
-   upon successfully parsing the input, or prints the parser error if it fails. *)
+   Scan and parse the input. Print out the AST. *)
 
 let () =
 	let usage_msg = "usage: ./toplevel.native [file.mc]" in
@@ -11,4 +10,4 @@ let () =
 	
 	let lexbuf = Lexing.from_channel !channel in
 	let ast = Parser.program Scanner.token lexbuf in
-	print_string "Success"
+	print_string (Ast.string_of_program ast) (* Print out the AST *)
