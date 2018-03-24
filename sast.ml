120ef847d0d00aa55502b050fe5908187eb55c8f
Mari Husain <maryamhusain98@gmail.com>
2018-03-24 15:11:43 -0400

    Added codgen, sast, semant

---
+++ sast.ml
@@ -0,0 +1,63 @@
+(* Semantically-checked Abstract Syntax Tree and functions for printing it *)
+
+open Ast
+
+type sexpr = typ * sx
+and sx =
+    SStringLiteral of string
+  | STableLiteral of (sexpr list) list
+  | SId of string
+  | SCall of string * sexpr list
+
+type svar_decl = SVarDecl of typ * string * sexpr
+
+type sstmt =
+  | SExpr of sexpr
+  | SStmtVDecl of svar_decl
+
+type sprogram = sstmt list
+
+(* Pretty-printing functions 
+
+let rec string_of_sexpr (t, e) =
+  "(" ^ string_of_typ t ^ " : " ^ (match e with
+    SLiteral(l) -> string_of_int l
+  | SBoolLit(true) -> "true"
+  | SBoolLit(false) -> "false"
+  | SFliteral(l) -> l
+  | SId(s) -> s
+  | SBinop(e1, o, e2) ->
+      string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2
+  | SUnop(o, e) -> string_of_uop o ^ string_of_sexpr e
+  | SAssign(v, e) -> v ^ " = " ^ string_of_sexpr e
+  | SCall(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
+  | SNoexpr -> ""
+				  ) ^ ")"				     
+
+let rec string_of_sstmt = function
+    SBlock(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_sstmt stmts) ^ "}\n"
+  | SExpr(expr) -> string_of_sexpr expr ^ ";\n";
+  | SReturn(expr) -> "return " ^ string_of_sexpr expr ^ ";\n";
+  | SIf(e, s, SBlock([])) ->
+      "if (" ^ string_of_sexpr e ^ ")\n" ^ string_of_sstmt s
+  | SIf(e, s1, s2) ->  "if (" ^ string_of_sexpr e ^ ")\n" ^
+      string_of_sstmt s1 ^ "else\n" ^ string_of_sstmt s2
+  | SFor(e1, e2, e3, s) ->
+      "for (" ^ string_of_sexpr e1  ^ " ; " ^ string_of_sexpr e2 ^ " ; " ^
+      string_of_sexpr e3  ^ ") " ^ string_of_sstmt s
+  | SWhile(e, s) -> "while (" ^ string_of_sexpr e ^ ") " ^ string_of_sstmt s
+
+let string_of_sfdecl fdecl =
+  string_of_typ fdecl.styp ^ " " ^
+  fdecl.sfname ^ "(" ^ String.concat ", " (List.map snd fdecl.sformals) ^
+  ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.slocals) ^
+  String.concat "" (List.map string_of_sstmt fdecl.sbody) ^
+  "}\n"
+
+let string_of_sprogram (vars, funcs) =
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_sfdecl funcs)
+*)
---
+++ semant.ml
@@ -0,0 +1,75 @@
+(* Semantic checking for the MicroC compiler *)
+
+open Ast
+open Sast
+
+module StringMap = Map.Make(String)
+
+(* Check to see if a variable is in the given symbol table. *)
+let rec find_variable (scope: symbol_table) name = 
+  try 
+    StringMap.find name scope.variables
+  with Not_found -> 
+    match scope parent with
+        Some(parent) -> find_variable parent name
+      | _ -> raise Not_found
+
+let convertToSAST (statements) = 
+
+  let symbols = 
+  {
+    variables = StringMap.empty;
+    parent = None;
+
+  } in
+
+  let functions =
+  {
+    variables = StringMap.empty;
+    parent = None;
+  } in
+
+  let type_of_id s =
+      try StringMap.find s symbols
+      with Not_found -> raise (Failure ("undeclared identifier " ^ s))
+  in
+
+  (* Return a function from our symbol table *)
+  let find_func s = 
+    try StringMap.find s function_decls
+    with Not_found -> raise (Failure ("unrecognized function " ^ s))
+  in
+
+  let rec convert_expr = function
+    StringLiteral s -> (String, SStringLiteral s)
+  | TableLiteral rows -> 
+    let check_row row = 
+      List.map convert_expr row
+    in (Table, STableLiteral(List.map check_row rows)) 
+  | Call(id, args) as call -> 
+      let fd = find_func fname in
+      let param_length = List.length fd.formals in
+      if List.length args != param_length then
+        raise (Failure ("expecting " ^ string_of_int param_length ^ 
+                              " arguments in " ^ string_of_expr call))
+      else let check_call (ft, _) e = 
+        let (et, e') = convert_expr e in 
+        let err = "illegal argument found " ^ string_of_typ et ^
+          " expected " ^ string_of_typ ft ^ " in " ^ string_of_expr e
+        in (check_assign ft et err, e')
+      in 
+      let args' = List.map check_call fd.formals args
+      in (fd.typ, SCall(fname, args'))
+
+  | Noexpr -> (Void, SNoexpr)
+  | Id s -> (type_of_id s, SId s)
+
+  in
+  let convert_vardecl (t, id, e) = (t, convert_expr e) (* TODO: Add checking for id *)
+  in 
+
+  let rec convert_statement = function
+    Expr e -> convert_expr e 
+  | StmtVDecl v -> SStmtVDecl(convert_vardecl v) (* Returns a SStmtVDecl *)
+  
+  in List.map convert_statement statements
